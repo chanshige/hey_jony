56a4ad6a8f1d62be33b4338f72cf65a57a71f0f2
tomtoru <tomtoru23@gmail.com>
2019-11-06 12:14:04 +0900

    jony transform a little

--- lib/hey_jony.rb
+++ lib/hey_jony.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 require 'dotenv/load'
-require 'http'
-require 'json'
-require 'eventmachine'
-require 'faye/websocket'
 
 require_relative 'hey_jony/version'
 require_relative 'hey_jony/notifier/slack'
@@ -14,36 +10,5 @@ module HeyJony
   def self.run
     slack = HeyJony::Notifier::Slack.new ENV['SLACK_INCOMING_WEBHOOK_URL'], ENV['SLACK_CHANNEL'], ENV['SLACK_USERNAME'], ENV['SLACK_ICON_EMOJI']
     slack.post 'Welcome to jony bot cli!'
-
-    # real time messaging api
-    response = HTTP.post("https://slack.com/api/rtm.start", params: {token: ENV['SLACK_API_TOKEN']})
-    url = JSON.parse(response.body)['url']
-
-    EM.run do
-      # JONY SERVER STAND BY!
-      websocket = Faye::WebSocket::Client.new(url)
-
-      websocket.on :open do
-        p ":::::SUCCESS TO START JONY SERVER:::::"
-      end
-      
-      websocket.on :message do |event|
-        event_data = JSON.parse(event.data)
-        p ["EVENT::", event_data]
-
-        if !event_data['text'].nil?
-          # jony checks his name ANYTIME ANYWHERE
-          if event_data['text'].include?('ジョニー')
-            slack.post '呼んだ？'
-          end
-        end
-      end
-
-      websocket.on :close do
-        p "......JONY IS DEAD......"
-        websocket = nil
-        EM.stop
-      end
-    end
   end
 end
