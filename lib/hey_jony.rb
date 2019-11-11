44f880deb7605c37c60a40170d180d1b414edb82
Shigeki Tanaka <dev@shigeki.tokyo>
2019-11-11 19:24:59 +0900

    add Thor

--- lib/hey_jony.rb
+++ lib/hey_jony.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 require 'dotenv/load'
+require 'thor'
 
 require 'hey_jony/version'
 require 'hey_jony/notifier/slack'
 
-# HeyJony
 module HeyJony
-  def self.run
-    slack = HeyJony::Notifier::Slack.new ENV['SLACK_INCOMING_WEBHOOK_URL'],
-                                         ENV['SLACK_CHANNEL'],
-                                         ENV['SLACK_USERNAME'],
-                                         ENV['SLACK_ICON_EMOJI']
-
-    slack.post 'Welcome to jony bot cli!'
+  class Cli < Thor
+    desc 'to_slack [message]', 'post message.'
+    # @param msg<String>
+    def to_slack(msg = 'Welcome to jony bot cli!')
+      slack = HeyJony::Notifier::Slack.new ENV['SLACK_INCOMING_WEBHOOK_URL'],
+                                           ENV['SLACK_CHANNEL'],
+                                           ENV['SLACK_USERNAME'],
+                                           ENV['SLACK_ICON_EMOJI']
+      slack.post msg
+    end
   end
 end
